c8d20eaec1e9e68e108f62a00b371fdefc563aa5
Keks <keks@htmlacademy.ru>
2022-03-22 10:46:11 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,9 @@
+import { createOffers } from './create-offers.js';
+import './popup.js';
+import './validation/validation-rooms-guests.js';
+import './validation/validation-price-title.js';
+import './page-activation/inactivated-page.js'
+import { inactivatedPage } from './page-activation/inactivated-page.js';
+
+createOffers();
+inactivatedPage();
